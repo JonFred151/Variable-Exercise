ec5d31030b44f3dec6d9765dfa9ca1fe782ebf78
Jon Frederick <jonFred151@gmail.com>
2023-11-21 14:13:08 -0600

    made my own Version

---
+++ Variable-Exercise/Variable-Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿// Declaring Variables and initialize variables
+string CatName = "Chubi";
+int CatAge = 10;
+char CatChar = 'C';
+bool IsMale = true;
+double CatWeight = 12.3;
+decimal Catlength = 3.2m;
+
+
+Console.WriteLine("My cat name is " + CatName + " he is " + CatAge + " years old. First letter in his name is " + CatChar + " He is a male cat " + IsMale + ". His weight and length is " + CatWeight + " " + Catlength);
